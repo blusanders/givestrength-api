46ae01f441a93dabf745ff1979e4f96e1bfec31a
blusanders <blusanders@yahoo.com>
2021-06-15 14:01:46 -0500

    Distance formula working correctly based on current user

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ VALUES (
     'Need Strength'
   );
 
-SELECT * from givestrapi_person
+SELECT latitude, longitude from givestrapi_person
 
 delete from givestrapi_persontype where id=3
 
@@ -11,6 +11,14 @@ delete from auth_user where id=13
 
 update auth_user set first_name = "Scott"
 where id=10
+
+update givestrapi_person set street = "1401 3rd Ave N" where id=6
+
+update givestrapi_person
+set
+latitude = 36.179260,
+longitude = -86.787727
+where id=6
 
 
 INSERT INTO givestrapi_day (description)
--- givestrapi/views/person.py
+++ givestrapi/views/person.py
@@ -8,6 +8,9 @@ from rest_framework import serializers
 from rest_framework import status
 from givestrapi.models import Person, PersonType
 from django.contrib.auth.models import User
+import urllib.request
+import json
+import math
 
 class PersonViewSet(ViewSet):
     """Give Your Strength Person"""
@@ -110,20 +113,48 @@ class PersonViewSet(ViewSet):
         """Handle GET requests to person resource
 
         Returns:
-            Response -- JSON serialized list of peopel within a given distance
+            Response -- JSON serialized list of people within a given distance
         """
         #get all people, filter by distance
-        user = Person.objects.get(user=request.auth.user)
-        person = Person.objects.all()
-        # geo = getGeo(street, city, state, zip)
-        # print("GEO: "+geo)
-
-        #    http://localhost:8000/person?distance=1
+        current_user = Person.objects.get(user=request.auth.user)
+        # print(current_user.user.first_name)
+        
+        #get all people, eventually filter by opposite of logged in person type
+        # person = Person.objects.all()
+        print(current_user.id)
+        person = Person.objects.exclude(user=request.auth.user)
+        print(person)
+        #calc distance bet lat/long and logged in user lat/long
+        #distance will come from querystring
+        #http://localhost:8000/person?distance=1
 
         distance = self.request.query_params.get('distance', None)
-        if distance is not None:
-            person_filtered = person.filter(gametype__id=game_type)
 
+        if distance is not None:
+
+            def distance_filter(person):
+                # from_lat = 36.17926
+                # from_long = -86.787727
+                from_lat = current_user.latitude
+                from_long = current_user.longitude
+                to_lat = person.latitude
+                to_long = person.longitude
+
+                distance_bet = get_distance(from_lat, from_long, to_lat, to_long)
+                print(person.id, distance_bet)
+                
+                if distance_bet<=float(distance):
+                    return True
+                return False
+
+            person = filter(distance_filter, person)
+
+        # print("Person Filtered:", person_filtered)
+
+        # person_type = person.person_type_id
+
+        # geo = getGeo(street, city, state, zip)
+        # print("GEO: "+geo)
 
         serializer = PersonSerializer(
             person, many=True, context={'request': request})
@@ -163,6 +194,46 @@ class PersonSerializer(serializers.ModelSerializer):
         )
         depth = 1
 
-def getGeoDistance(lat1,lat2,lat3,lat4):
-    return "HELLO"
+def getHelp() :
+    from_address = [36.17915, -86.75908] #logged in user
 
+    #all TN addresses
+    #ask Will about filtering data first
+    #searching all addresses not realistic
+    help_array = [
+        [36.17213, -86.75485], #.53
+        [36.1844, -86.74291], #.96
+        [36.18869, -86.76962], #.88	
+        [36.196770, -86.745057] #1.44
+    ]
+
+    distance=1
+    within_distance = []
+
+    for latlong in range(len(help_array)):
+        distance_away = get_distance(
+        from_address[0],
+        from_address[1],
+        help_array[latlong][0],
+        help_array[latlong][1]
+        )
+        if distance_away<=distance:
+            within_distance.append(help_array[latlong])
+    print(within_distance)
+
+def get_distance(from_lat,from_long, to_lat, to_long):
+
+    #takes to and from lat/long
+    #returns distance in miles bet those points
+
+    radius = 6371  # km
+
+    dlat = math.radians(to_lat - from_lat)
+    dlon = math.radians(to_long - from_long)
+    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+         math.cos(math.radians(from_lat)) * math.cos(math.radians(to_lat)) *
+         math.sin(dlon / 2) * math.sin(dlon / 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = radius * c
+    d = d*.62 #returns miles
+    return d
