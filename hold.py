66f2f3b3f7971686c4636634bbff7b5581c432a6
blusanders <blusanders@yahoo.com>
2021-06-20 16:28:31 -0500

    Icon swap working

--- hold.py
+++ hold.py
@@ -42,3 +42,42 @@ zip: "79932"
 https://nominatim.openstreetmap.org/search?q=830+Windhurst+San+Antonio+TX&format=geojson
 
 
+
+
+
+def getHelp() :
+    from_address = [36.17915, -86.75908] #logged in user
+
+    #all TN addresses
+    #ask Will about filtering data first
+    #searching all addresses not realistic
+    help_array = [
+        [36.17213, -86.75485], #.53
+        [36.1844, -86.74291], #.96
+        [36.18869, -86.76962], #.88	
+        [36.196770, -86.745057] #1.44
+    ]
+
+    distance=1
+    within_distance = []
+
+    for latlong in range(len(help_array)):
+        distance_away = get_distance(
+        from_address[0],
+        from_address[1],
+        help_array[latlong][0],
+        help_array[latlong][1]
+        )
+        if distance_away<=distance:
+            within_distance.append(help_array[latlong])
+    print(within_distance)
+
+
+def destroy(self, request, pk=None):
+        """Handle DELETE requests for a story
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            story = Story.objects.get(user=request.auth.user,pk=pk)
+            story.delete()
